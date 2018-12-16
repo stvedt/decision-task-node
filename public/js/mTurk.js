13ee8c75a19ec61535b24100c7cccf694264b2dc
Stephen Tvedt <stephen@stephentvedt.com>
2018-12-16 15:05:09 -0500

    adding the javascript that is used in mTurk to repo for tracking

---
+++ public/js/mTurk.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+
+  //postmessage
+  function receiveMessage(event){
+    console.log('task completed from iframe yay!');
+    // Do we trust the sender of this message? Check origin
+    //if (event.origin !== "http://example.com")
+    //  return;
+
+    // event.source is popup/iframe
+    // event.data is "the text of the actual message"
+    document.querySelector('#submitButton').style.display = 'block';;
+  }
+  window.addEventListener("message", receiveMessage, false);
+
+};//end window.onload
