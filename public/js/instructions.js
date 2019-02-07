10d9a6b45bfc3c8ba13464de52231df6e54e6cfb
Stephen Tvedt <stephen@stephentvedt.com>
2019-02-06 21:22:28 -0500

    remove consent form logic that is no longer being used and was blocking js

--- public/js/instructions.js
+++ public/js/instructions.js
@@ -1,13 +1,4 @@
 (function(){
-  document.getElementById('consent').addEventListener('change', function(event){
-    var $nextProblem = document.getElementById('next-problem');
-    if(event.target.checked){
-      $nextProblem.classList.remove('disabled');
-    } else {
-      $nextProblem.classList.add('disabled');
-    }
-  });
-
   function getPageJSONData(){
     fetch("/js/pages.json", {
       method: "GET"
